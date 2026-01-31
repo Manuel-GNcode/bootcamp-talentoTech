94182f9f9a3a69bc29a35751052a03b8c8da1819
GNcode <gngalarga027@gmail.com>
2026-01-30 20:31:52 -0500

    avance de la api

--- front/mi-primer-angular/src/app/app.routes.ts
+++ front/mi-primer-angular/src/app/app.routes.ts
@@ -5,5 +5,4 @@ import { App } from './app';
 export const routes: Routes = [
   {path: "", redirectTo: "", pathMatch: "full"},
   {path: "users", component: UserComponent},
-  {path: "**", component: UserComponent}
 ];
